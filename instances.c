72b803fb35a6312f3c553684e202093cb6d86711
Wesley Peters <wespeters18@gmail.com>
2026-08-23 10:11:10 -0400

    fun with gravity

--- instances.c
+++ instances.c
@@ -33,13 +33,18 @@ bool instances_init(instance_data *instances)
 
         instances->bodies[i] = b3CreateBody(instances->world, &body_def);
 
-        b3BoxHull box = b3MakeBoxHull(scale[0], scale[1], scale[2]);
-
         b3ShapeDef shape_def = b3DefaultShapeDef();
         shape_def.density = 1.f;
         shape_def.baseMaterial.friction = 0.1f;
 
+        b3BoxHull box = b3MakeBoxHull(scale[0] * 0.5, scale[1] * 0.5f, scale[2] * 0.5f);
+
         b3CreateHullShape(instances->bodies[i], &shape_def, &box.base);
+
+        vec3 velocity = { random_float(), random_float(), random_float() };
+        glm_vec3_scale(velocity, 2.f, velocity);
+        glm_vec3_subs(velocity, 1.f, velocity);
+        b3Body_SetLinearVelocity(instances->bodies[i], *(b3Vec3*)&velocity);
     }
 
     return true;
@@ -47,6 +52,49 @@ bool instances_init(instance_data *instances)
 
 void instances_update(instance_data *instances)
 {
+    // apply gravity
+    vec3 p_i, p_j;
+    float m_i, m_j;
+
+    const float g = 0.5f;
+
+    for(u32 i=0; i<MAX_INSTANCES; i++)
+    {
+        b3WorldTransform transform_i = b3Body_GetTransform(instances->bodies[i]);
+        m_i = b3Body_GetMass(instances->bodies[i]);
+
+        for(u32 j=i+1; j<MAX_INSTANCES; j++)
+        {
+            b3WorldTransform transform_j = b3Body_GetTransform(instances->bodies[j]);
+            m_j = b3Body_GetMass(instances->bodies[j]);
+            
+            // calculate force
+            float dir_x = transform_j.p.x - transform_i.p.x; 
+            float dir_y = transform_j.p.y - transform_i.p.y; 
+            float dir_z = transform_j.p.z - transform_i.p.z; 
+
+            float dis2 = dir_x * dir_x;
+            dis2 += dir_y * dir_y;
+            dis2 += dir_z * dir_z;
+
+            float grav = g * m_i * m_j / dis2;
+
+            dis2 = sqrt(dis2);
+            dis2 = 1.f / dis2;
+
+            dir_x *= dis2;
+            dir_y *= dis2;
+            dir_z *= dis2;
+
+            b3Vec3 f_i = {  dir_x * grav,  dir_y * grav,  dir_z * grav };
+            b3Vec3 f_j = { -dir_x * grav, -dir_y * grav, -dir_z * grav };
+
+            b3Body_ApplyForceToCenter(instances->bodies[i], f_i, true);
+            b3Body_ApplyForceToCenter(instances->bodies[j], f_j, true);
+        }
+    }
+
+    //
     static const float time_step = 1.f / 60.f;
     int sub_step_count = 4;
 
--- instances.h
+++ instances.h
@@ -6,7 +6,7 @@
 
 #include "box3d/box3d.h"
 
-#define MAX_INSTANCES 8000
+#define MAX_INSTANCES 1000
 
 typedef struct instance_data_t
 {
